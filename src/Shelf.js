7a61e2605b4872b27c7e640add2681acf7d46f42
Henrique de Almeida <henriquealmeida1492@gmail.com>
2019-02-14 13:26:19 -0200

    refactor: introduce component Shelf in App.js

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class Shelf extends Component{
 
 	render(){
+		const { shelfTitle, books } = this.props
 		return(
-
+			<div className="bookshelf">
+				<h2 className="bookshelf-title">{shelfTitle}</h2>
+				<div className="bookshelf-books">
+					<ol className="books-grid">
+						{books.map(book => (
+								<li>
+									<Book title={book.bookTitle} authors={book.authors} cover={book.cover}/>
+								</li>
+							))}
+					</ol>
+				</div>
+			</div>
 		)
 	}
 }
